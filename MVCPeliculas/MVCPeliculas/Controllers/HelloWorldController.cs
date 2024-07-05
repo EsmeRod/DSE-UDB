e4d9aa12cd28c6a2e74848b474e872c52e3b870e
EsmeRod <RS200052@alumno.udb.edu.sv>
2024-07-04 18:48:41 -0600

    Guia 1 Ejemplos

---
+++ MVCPeliculas/MVCPeliculas/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCPeliculas.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /helloworld/Index/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: /helloworld/Welcome/
+        public ActionResult Welcome(string Nombre, int numveces = 1)
+        {
+            ViewData["Nombre"] = "Hola" + Nombre;
+            ViewData["numveces"] = numveces;
+            return View();
+        }
+    }
+}
---
+++ MVCPeliculas/MVCPeliculas/Models/PeliculasDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCPeliculas.Models
+{
+    public class PeliculasDBContext: DbContext
+    {
+        public PeliculasDBContext(DbContextOptions option): base(option) 
+        {
+        }
+        public DbSet<pelicula> Peliculas { get; set; } 
+    }
+}
